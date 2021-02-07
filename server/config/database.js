2bb0f488dc4054814682476008fc8943d4b0d009
Jair Anguiano <jairanpo@gmail.com>
2021-02-07 13:37:05 -0600

    Docker compose with NGINX

--- server/config/database.js
+++ server/config/database.js
@@ -1,23 +1,41 @@
+const keys = require('../keys');
 const mongoose = require('mongoose');
-require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({path: 'variables.env'});
 
-const conectarDB = async () => {
-    try{
-        await mongoose.connect('mongodb://mongo:27017/database', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-        })
+const username = keys.mongoUsername;
+const password = keys.mongoPassword;
+const host = keys.mongoHost;
+const database = keys.mongoDatabase;
+
+var connection_string = null;
+if (process.env.MODE == 'pro'){
+    connection_string = `mongodb://${username}:${password}@${host}/${database}`;
+}
+else{
+    connection_string = 'mongodb://tamashii-mongo/database';
+}
+
+
+console.log(connection_string)
+const connection = async function () {
+    try {
+        await mongoose.connect(
+            connection_string,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false,
+                useCreateIndex: true
+            })
         console.log('MongoDB its connected');
 
-    } catch (error){
+    } catch (error) {
         console.log('There was an error connecting to MongoDB');
         console.log(error);
         process.exit(1);// stop the app
     }
 }
 
-module.exports = conectarDB;
+module.exports = connection;
 
 
