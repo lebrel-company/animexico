b1a331d23b927dc454726b81069ff99a38dfa0ea
Jair Anguiano <jairanpo@gmail.com>
2021-03-05 16:23:45 -0600

    Gradient for footer and header

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,10 @@ import routes from '../utils/routes'
 
 function Footer() {
     return (
-        <div className='bg-dark text-pale font-simp shadow-lg w-screen'>
+        <div className='
+        bg-dark text-pale font-simp shadow-lg w-screen
+        bg-gradient-to-t from-dark to-darkindigo'
+        >
             <div className='container mx-auto p-8'>
                 <div className='flex flex-1 justify-center items-center'>
                     <div className='mx-auto'>
