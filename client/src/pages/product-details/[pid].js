3d46c57cfd3797cd63b846e72844f0e25fd6f7c7
Emmanuel <mendoza.emano@gmail.com>
2021-03-20 14:41:46 -0600

    dropzone add images

---
+++ client/src/pages/product-details/[pid].js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {useRouter} from 'next/router'
+
+
+function ProductDetails() {
+    let router = useRouter()
+    let {pid} = router.query
+
+
+    return (
+        <div>
+        
+       </div>
+    )
+}
+
+export default ProductDetails
