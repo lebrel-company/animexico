b0ba52f088664ea78ccb2e7c5421b5a4013d96cf
Jair Anguiano <jairanpo@gmail.com>
2021-04-02 18:57:40 -0600

    redo-formik

--- client/src/controllers/signup/signup.formik.js
+++ client/src/controllers/signup/signup.formik.js
@@ -1,6 +1,6 @@
 'use strict';
 // libraries:
-import {useFormik} from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 // -- -- -- -- -- -- -- -- -- -- -- -- -- --
 // Contexts:
@@ -10,28 +10,68 @@ import * as Yup from "yup";
 // components:
 // -- -- -- -- -- -- -- -- -- -- -- -- -- --
 // project:
-import {userFields} from "../../utils/fields/user";
+import { userFields } from "../../utils/fields/user";
 
 //==============================================================================
 
 
 export function signupFormik(mutation, states, contexts, route) {
 
-    var _listOfValueNames = Object.keys(userFields)
+    var _listOfValueNames = Object.keys(userFields);
 
 
     function createInitialValuesObject() {
-        let result = {}
+        let result = {};
         _listOfValueNames.forEach(function (_key) {
-            result[_key] = ''
-        })
-        return result
+            result[_key] = '';
+        });
+        return result;
     }
 
+    function requiredMessage(field_name){
+        return `${field_name} es requerido`
+    }
 
     return useFormik({
             initialValues: createInitialValuesObject(),
-            validationSchema: Yup.object(),
+            validationSchema: Yup.object({
+                firstName: Yup.string()
+                    .required(requiredMessage(userFields.firstName.placeholder)),
+                lastName: Yup.string()
+                    .required(requiredMessage(userFields.lastName.placeholder)),
+                secondLastName: Yup.string()
+                    .required(requiredMessage(userFields.secondLastName.placeholder)),
+                email: Yup.string()
+                    .required(requiredMessage(userFields.email.placeholder))
+                    .email('El correo no es válido'),
+                password: Yup.string()
+                    .required(requiredMessage(userFields.password.placeholder)),
+                passwordConfirmation: Yup.string()
+                    .required(requiredMessage(userFields.passwordConfirmation.placeholder)),
+                birthday: Yup.string()
+                    .required(requiredMessage(userFields.birthday.placeholder)),
+                cellphone:
+                    Yup.string()
+                        .required(requiredMessage(userFields.cellphone.placeholder)),
+                address: Yup.object({
+                    country: Yup.string()
+                        .required(requiredMessage(userFields.address.country.placeholder)),
+                    city: Yup.string()
+                        .required(requiredMessage(userFields.address.city.placeholder)),
+                    state: Yup.string()
+                        .required(requiredMessage(userFields.address.state.placeholder)),
+                    zipcode: Yup.number()
+                        .required(requiredMessage(userFields.address.zipcode.placeholder)),
+                    neighbourhood: Yup.string()
+                        .required(requiredMessage(userFields.address.neighbourhood.placeholder)),
+                    street: Yup.string()
+                        .required(requiredMessage(userFields.address.street.placeholder)),
+                    buildingNumber: Yup.string()
+                        .required(requiredMessage(userFields.address.buildingNumber.placeholder)),
+                    apartmentNumber: Yup.string()
+                        .required(requiredMessage(userFields.address.apartmentNumber.placeholder))
+                })
+            }),
             onSubmit: async function submitForm(values) {
                 let _input = {
                     firstName: values.firstName,
@@ -54,26 +94,22 @@ export function signupFormik(mutation, states, contexts, route) {
                             apartmentNumber: values.apartmentNumber
                         }
                     }
-                }
+                };
                 try {
                     let {data} = await mutation({
                         variables: {
                             input: _input
                         }
                     });
-                    contexts.authContext.setAuthState(data.signup)
+                    contexts.authContext.setAuthState(data.signup);
                     setTimeout(() => {
                         route.hook.push(route.path);
                     }, 800);
                 } catch (error) {
-                    console.error(error)
+                    console.error(error);
                 }
-            },
+            }
         }
     );
-}
-
-
-    return result
 }
 
