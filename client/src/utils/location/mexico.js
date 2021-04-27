c6f48944763e5e927c687a367f29c178ca2e7a90
Jair Anguiano <jairanpo@gmail.com>
2021-04-26 19:55:39 -0500

    Adding mexican states

---
+++ client/src/utils/location/mexico.js
@@ -0,0 +1,42 @@
+'use strict';
+// libraries:
+// -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// models:
+// -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// project:
+//==============================================================================
+
+export var listOfStates = [
+    'Aguascalientes',
+    'Baja California',
+    'Baja California Sur',
+    'Campeche',
+    'Chiapas',
+    'Chihuahua',
+    'Coahuila',
+    'Colima',
+    'Ciudad de México',
+    'Durango',
+    'Guanajuato',
+    'Guerrero',
+    'Hidalgo',
+    'Jalisco',
+    'Estado de Mexico',
+    'Michoacan',
+    'Morelos',
+    'Nayarit',
+    'Nuevo Leon',
+    'Oaxaca',
+    'Puebla',
+    'Queretaro',
+    'Quintana Roo',
+    'San Luis Potosi',
+    'Sinaloa',
+    'Sonora',
+    'Tabasco',
+    'Tamaulipas',
+    'Tlaxcala',
+    'Veracruz',
+    'Yucatan',
+    'Zacatecas',
+];
